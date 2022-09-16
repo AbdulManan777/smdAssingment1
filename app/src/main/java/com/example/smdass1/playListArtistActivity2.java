57bdb04823722e82fef787df2ed7d2bba897c863
Abdul Manan <i190500@nu.edu.pk>
2022-09-16 19:27:30 +0500

    5 screens done

---
+++ app/src/main/java/com/example/smdass1/playListArtistActivity2.java
@@ -0,0 +1,14 @@
+package com.example.smdass1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playListArtistActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_list_artist2);
+    }
+}
