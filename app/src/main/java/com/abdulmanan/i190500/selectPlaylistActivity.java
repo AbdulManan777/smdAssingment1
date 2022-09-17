ee861b49d3ccdf5fce095b94821512d5c4d3f739
Abdul Manan <i190500@nu.edu.pk>
2022-09-17 19:40:45 +0500

    11, 12, 13 Screens Done

---
+++ app/src/main/java/com/abdulmanan/i190500/selectPlaylistActivity.java
@@ -0,0 +1,14 @@
+package com.abdulmanan.i190500;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selectPlaylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_playlist);
+    }
+}
