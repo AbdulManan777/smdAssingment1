efecd303336c4429a64c19750426ce2a86e6fdfb
Abdul Manan <i190500@nu.edu.pk>
2022-09-17 14:06:47 +0500

    12 Screens Done

---
+++ app/src/main/java/com/abdulmanan/i190500/recordingMusicWithTimerActivity.java
@@ -0,0 +1,14 @@
+package com.abdulmanan.i190500;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recordingMusicWithTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording_music_with_timer);
+    }
+}
