46cf2796f34e3c7058dca55562f9f55042724264
Abdul Manan <i190500@nu.edu.pk>
2022-09-18 12:04:15 +0500

    16 & 17 Screens Done

---
+++ app/src/main/java/com/abdulmanan/i190500/songTitlePlayingActivity.java
@@ -0,0 +1,14 @@
+package com.abdulmanan.i190500;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class songTitlePlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_title_playing);
+    }
+}
